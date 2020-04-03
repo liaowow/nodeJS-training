d4edc8363c9c91f06d5cd4d784c1a24c4ddeece5
Annie Liao <liaowow@gmail.com>
2020-04-03 15:23:33 -0400

    added explainer on Buffer

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ let { guessNumber } = require("./numGame")
 process.stdout.write(`I am holding a number from 1 through 10. Guess which number I have? \n(Write \"quit\" to exit game)\n`)
 
 let playGame = (userInput) => {
+    // console.log("userInput before converting to string: ", typeof userInput)
+    /** 
+    The userInput we receive is an instance of the Node Buffer class (i.e. an object), so we convert it to a string before printing.
+    In Node.js, Buffer objects are used to represent binary data in the form of a sequence of bytes.
+    **/
     let input = userInput.toString().trim()
     guessNumber(input)
 }
